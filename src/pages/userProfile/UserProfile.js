2697b32ce7c0c113857b03b1cdece4558d2a4ec6
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-18 16:38:01 +0100

    Add the delete all functions

--- src/pages/userProfile/UserProfile.js
+++ src/pages/userProfile/UserProfile.js
@@ -2,37 +2,38 @@ import React, { useState } from "react";
 import { BiTrash } from "react-icons/bi";
 import { userData } from "../../data/UserData";
 
-const UserProfile = ({ id, image, name, job }) => {
+const UserProfile = () => {
   const [users, setUsers] = useState(userData);
 
-  const removeUser = () => {};
-
+  const removeUser = (id) => {
+    const newUser = users.filter((user) => user.id !== id);
+    setUsers(newUser);
+  };
   return (
-    <>
+    <div>
       {users.map((user) => {
         const { id, name, img, job } = user;
         return (
           <div key={id}>
             <div>
-              <img src={img} alt="" />
+              <img src={img} alt="{name}" />
             </div>
+
             <div>
               <h1>{name}</h1>
               <p>{job}</p>
             </div>
+
             <div>
-              <button>
-                {" "}
-                <BiTrash color="red" size={22} onClick={() => removeUser(id)} />
+              <button className="btn btn-danger">
+                <BiTrash onClick={() => removeUser(id)} />
               </button>
             </div>
           </div>
         );
       })}
-      <button className="--btn --btn-danger" onClick={() => setUsers([])}>
-        Delete All
-      </button>
-    </>
+      <button onClick={() => setUsers([])}>Remove All</button>
+    </div>
   );
 };
 
