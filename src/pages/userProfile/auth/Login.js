cd6e590b72146d2753c32345dff4722b4d4982d4
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-21 19:14:05 +0100

    Add login page

---
+++ src/pages/userProfile/auth/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import loginImg from "../../assets/login.svg";
+
+const Login = () => {
+  return (
+    <div className="main-container --flex-center">
+      <div className="img-container">
+        <img src={loginImg} alt="login" />
+      </div>
+      <div className="form-container">
+        <form className="--form-control">
+          <h2 className="--color-danger --text-center">Login</h2>
+          <input type="text" className="--width-100" placeholder="Username" />
+          <div className="password">
+            <input className="--width-100" placeholder="Password" />
+          </div>
+          <button className="--btn --btn-primary --btn-block">Login</button>
+          <a href="#" className="--text-sm">
+            Forgot password
+          </a>
+          <span className="--text-sm --block">
+            Don't have an account?{" "}
+            <a href="#" className="--text-sm">
+              Register
+            </a>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/userProfile/auth/Register.js
@@ -0,0 +1,33 @@
+// import React from "react";
+// import registerImg from "../../assets/register.svg";
+
+// const Register = () => {
+//   return (
+//     <div className="main-container --flex-center">
+//       <div className="img-container">
+//         <img src={registerImg} alt="register" />
+//       </div>
+//       <div className="form-container">
+//         <form className="--form-control">
+//           <h2 className="--color-danger --text-center">Register</h2>
+//           <input type="text" className="--width-100" placeholder="Username" />
+//           <div className="password">
+//             <input className="--width-100" placeholder="Password" />
+//           </div>
+//           <button className="--btn --btn-primary --btn-block">Register</button>
+//           <a href="#" className="--text-sm">
+//             Forgot password
+//           </a>
+//           <span className="--text-sm --block">
+//             Don't have an account?{" "}
+//             <a href="#" className="--text-sm">
+//               Register
+//             </a>
+//           </span>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Register;
