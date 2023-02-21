844516d96ff75efeb6669aec37d826c5a74b6414
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-21 19:59:31 +0100

    Add close icon

--- src/pages/userProfile/auth/AuthContainer.js
+++ src/pages/userProfile/auth/AuthContainer.js
@@ -2,13 +2,15 @@ import React from "react";
 import Login from "./Login";
 import "./AuthContainer.scss";
 import Register from "./Register";
+import Reset from "./Reset";
 
 const AuthContainer = () => {
   return (
     <section className="--flex-center --100vh">
       <div className="container box">
         {/* <Login /> */}
-        <Register />
+        {/* <Register /> */}
+        <Reset />
       </div>
     </section>
   );
