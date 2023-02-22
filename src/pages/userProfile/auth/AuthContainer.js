4a06720107b0adf16ea3696a6bd50259c5226b82
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-22 15:13:25 +0100

    Add the login conditionally

--- src/pages/userProfile/auth/AuthContainer.js
+++ src/pages/userProfile/auth/AuthContainer.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "./Login";
 import "./AuthContainer.scss";
 import Register from "./Register";
 import Reset from "./Reset";
 
 const AuthContainer = () => {
+  const [login, setLogin] = useState(true);
+  const [register, setRegister] = useState(false);
+  const [reset, setReset] = useState(false);
   return (
     <section className="--flex-center --100vh">
       <div className="container box">
-        {/* <Login /> */}
+        {login && <Login />}
+
         {/* <Register /> */}
         <Reset />
       </div>
