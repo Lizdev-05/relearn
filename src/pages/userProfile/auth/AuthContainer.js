724bb515112b28e106ca64f31c47723fd477cacc
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-22 15:16:42 +0100

    link to register from login

--- src/pages/userProfile/auth/AuthContainer.js
+++ src/pages/userProfile/auth/AuthContainer.js
@@ -8,10 +8,15 @@ const AuthContainer = () => {
   const [login, setLogin] = useState(true);
   const [register, setRegister] = useState(false);
   const [reset, setReset] = useState(false);
+
+  const registerHandler = () => {
+    setLogin(false);
+    setRegister(true);
+  };
   return (
     <section className="--flex-center --100vh">
       <div className="container box">
-        {login && <Login />}
+        {login && <Login onRegister={registerHandler} />}
         {register && <Register />}
         {reset && <Reset />}
       </div>
