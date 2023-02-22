06192438c35972576bb54804d846054d4673fe11
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-22 15:11:18 +0100

    Add onclick event to the resest and register

--- src/pages/userProfile/auth/Login.js
+++ src/pages/userProfile/auth/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import loginImg from "../../../assets/login.svg";
 import "./AuthContainer.scss";
 
-const Login = () => {
+const Login = ({ onRegister, onReset }) => {
   return (
     <div className="main-container --flex-center">
       <div className="img-container">
@@ -16,12 +16,12 @@ const Login = () => {
             <input className="--width-100" placeholder="Password" />
           </div>
           <button className="--btn --btn-primary --btn-block">Login</button>
-          <a href="#" className="--text-sm">
+          <a href="#" className="--text-sm" onClick={onReset}>
             Forgot password
           </a>
           <span className="--text-sm --block">
             Don't have an account?{" "}
-            <a href="#" className="--text-sm">
+            <a href="#" className="--text-sm" onClick={onRegister}>
               Register
             </a>
           </span>
