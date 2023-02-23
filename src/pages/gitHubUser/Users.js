c8d9e97f5ebeec1af70db14eb5f81e6dd77bb69d
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-23 21:08:03 +0100

    Add the loading state

--- src/pages/gitHubUser/Users.js
+++ src/pages/gitHubUser/Users.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
-import profile1 from "../../assets/profile1.png";
+import spinner from "../../assets/spinner.jpg";
 
 const Users = () => {
   const [user, setUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const userUrl = "https://api.github.com/users";
 
   const getUser = async () => {
+    setIsLoading(true);
     const response = await fetch(userUrl);
     const data = await response.json();
     setUser(data);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -30,20 +33,24 @@ const Users = () => {
       <div className="container">
         <h2 className="--text-center --text-light">GitHub Users</h2>
         <div className="--line"></div>
-        <div className="--grid-25 --py">
-          {user.map((u) => {
-            const { avatar_url, login, id, html_url } = u;
-            return (
-              <div className="--card --bg-light --p --flex-start" key={id}>
-                <img src={avatar_url} alt="" className="--profile-img --mx" />
-                <span>
-                  <h2>{login} </h2>
-                  <a href={html_url}>view profile</a>
-                </span>
-              </div>
-            );
-          })}
-        </div>
+        {isLoading ? (
+          <img src={spinner} alt="loading" />
+        ) : (
+          <div className="--grid-25 --py">
+            {user.map((u) => {
+              const { avatar_url, login, id, html_url } = u;
+              return (
+                <div className="--card --bg-light --p --flex-start" key={id}>
+                  <img src={avatar_url} alt="" className="--profile-img --mx" />
+                  <span>
+                    <h2>{login} </h2>
+                    <a href={html_url}>view profile</a>
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </section>
   );
