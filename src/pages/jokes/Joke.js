dae0118e68332a5ae903cba09238c5a5e0da192c
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-23 20:00:09 +0100

    Add loading state

--- src/pages/jokes/Joke.js
+++ src/pages/jokes/Joke.js
@@ -3,6 +3,7 @@ import spinner from "../../assets/spinner.jpg";
 
 const Joke = () => {
   const [joke, setJoke] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const jokeUrl = "https://api.chucknorris.io/jokes/random";
 
@@ -14,6 +15,7 @@ const Joke = () => {
       .then((data) => {
         console.log(data);
         setJoke(data);
+        setIsLoading(false);
       });
   }, []);
 
@@ -22,9 +24,17 @@ const Joke = () => {
       <div className="container --card --bg-light --p">
         <h2 className="">Random Jokes Generator</h2>
         <h4 className="--line"></h4>
-        <h4>Joke id: {joke.id}</h4>
+        {isLoading ? (
+          <div className="--center-all">
+            <img src={spinner} alt="loading" width={100} />
+          </div>
+        ) : (
+          <>
+            <h4>Joke id: {joke.id}</h4>
+            <p>{joke.value}</p>
+          </>
+        )}
 
-        <p>{joke.value}</p>
         <br />
         <button className="--btn --btn-primary">Generate Jokes</button>
       </div>
