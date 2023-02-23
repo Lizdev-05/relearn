79c99f804ab5ed80b158e49291ccdaa8f6e7787a
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-23 20:28:14 +0100

    Fetch the data

--- src/pages/gitHubUser/Users.js
+++ src/pages/gitHubUser/Users.js
@@ -3,17 +3,17 @@ import React, { useEffect, useState } from "react";
 const Users = () => {
   const [user, setUser] = useState({});
 
-  //   const jokeUrl = "https://api.chucknorris.io/jokes/random";
+  const jokeUrl = "https://api.chucknorris.io/jokes/random";
 
-  //   useEffect(() => {
-  //     fetch(jokeUrl)
-  //       .then((response) => {
-  //         return response.json();
-  //       })
-  //       .then((data) => {
-  //         console.log(data);
-  //       });
-  //   }, []);
+  useEffect(() => {
+    fetch(jokeUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
 
   return (
     <section className="--flex-center --bg-primary --p --100vh">
