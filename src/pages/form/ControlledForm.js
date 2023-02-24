833ed4a080741a8e070996c8d62283be9dbf5d49
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-02-24 19:26:01 +0100

    Add onclick to the input

--- src/pages/form/ControlledForm.js
+++ src/pages/form/ControlledForm.js
@@ -7,24 +7,38 @@ const ControlledForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(name, job);
+    setName("");
+    setJob("");
   };
   return (
     <div className="--bg-primary --mh-100vh">
       <h1 className="--flex-center"> Controlled Form</h1>
       <div className="--card --width-500px --bg-light --flex-center --center-all">
         <div className=" --flex-center">
-          <form action="">
+          <form action="" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name" name="name">
                 Name
               </label>
-              <input type="text" />
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
             </div>
             <div>
               <label htmlFor="job" name="job">
                 Job
               </label>
-              <input type="text" />
+              <input
+                type="text"
+                value={job}
+                onChange={(e) => {
+                  setJob(e.target.value);
+                }}
+              />
             </div>
           </form>
         </div>
