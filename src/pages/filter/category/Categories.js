d344e0e3615ae27f60176087457e0ab1e6abbaf8
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-04-13 14:14:24 +0100

    Add categories boilerplate and its style

--- src/pages/filter/category/Categories.js
+++ src/pages/filter/category/Categories.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./Categories.css";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div>
+      <button type="button" className="btn --btn --btn-secondary">
+        All
+      </button>
+    </div>
+  );
 };
 
 export default Categories;
