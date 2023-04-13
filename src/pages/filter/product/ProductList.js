790dd4228112ac88e3ae070629bc4a2d16b4aa20
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-04-13 09:33:19 +0100

    Complete ProductList styling

--- src/pages/filter/product/ProductList.js
+++ src/pages/filter/product/ProductList.js
@@ -9,17 +9,17 @@ const ProductList = () => {
     <div>
       <div className="header">
         <header className="container">
-          <h2 className="--color-white">
+          <h2 className="--color-white --text-center">
             <span className="color-green ">Product</span> Filter
           </h2>
-          <div>
+          <div className="--flex-between --flex-dir-column --py">
             <Search />
             <Categories />
           </div>
         </header>
       </div>
       <div className="product-container">
-        <div className="container">
+        <div className="product container --grid-25 --py2 ">
           <Product />
         </div>
       </div>
