a37803474fde6d08f24b63aed767091d939f8116
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-04-13 20:21:32 +0100

    Map dynamically

--- src/pages/filter/product/Product.js
+++ src/pages/filter/product/Product.js
@@ -1,26 +1,17 @@
 import React from "react";
 import "./Product.css";
-import { products } from "../../../products-data";
-import product1 from "../images/7.png";
 
-const Product = () => {
+const Product = ({ img, title, price }) => {
   return (
     <div className="product --card">
-      {products.map((product) => {
-        let { id, title, price, category, img } = product;
-        return (
-          <div key={id}>
-            <img src={img} alt="product image" />
-            <div className="content --center-all --p">
-              <h3 className="--text-light">{title}s</h3>
-              <div className="--flex-between --width-100">
-                <span className="--text-light">{price}</span>
-                <button className="--btn -btn-danger">Add to Cart</button>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+      <img src={img} alt="product image" />
+      <div className="content --center-all --p">
+        <h3 className="--text-light">{title}</h3>
+        <div className="--flex-between --width-100">
+          <span className="--text-light">{price}</span>
+          <button className="--btn -btn-danger">Add to Cart</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/filter/product/ProductList.js
+++ src/pages/filter/product/ProductList.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Categories from "../category/Categories";
 import Product from "../product/Product";
 import Search from "../search/Search";
 import "./ProductList.css";
+import { products as items } from "../../../products-data";
 
 const ProductList = () => {
+  const [products, setProducts] = useState(items);
   return (
     <div>
       <div className="header">
@@ -12,7 +14,7 @@ const ProductList = () => {
           <h2 className="--color-white --text-center">
             <span className="color-green ">Product</span> Filter
           </h2>
-          <div className="--flex-between --flex-dir-column --py">
+          <div className="--flex-between ---flex-dir-column--py">
             <Search />
             <Categories />
           </div>
@@ -20,7 +22,14 @@ const ProductList = () => {
       </div>
       <div className="product-container">
         <div className="products container --grid-25 --py2 ">
-          <Product />
+          {products.map((product) => {
+            const { id, title, price, category, img } = product;
+            return (
+              <div key={id}>
+                <Product title={title} price={price} img={img} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
