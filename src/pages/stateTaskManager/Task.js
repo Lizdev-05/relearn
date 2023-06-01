b1c4a7dad67b998c1896e30dc2fc6626e3eb30aa
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-01 20:04:51 +0100

    Link staye to form

--- src/pages/stateTaskManager/Task.js
+++ src/pages/stateTaskManager/Task.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { FaCheckDouble, FaEdit, FaTrashAlt } from "react-icons/fa";
 
 const Task = () => {
-  return <div>Task</div>;
+  return (
+    <div className="task">
+      <span>
+        <p>
+          <b>Task:</b> Task One
+        </p>
+        <p>
+          <b>Date:</b> 31/5/2023
+        </p>
+      </span>
+
+      <span>
+        <button>
+          <FaEdit color="green" />
+        </button>
+        <button>
+          {" "}
+          <FaTrashAlt color="red" />
+        </button>
+        <button>
+          {" "}
+          <FaCheckDouble color="purple" />
+        </button>
+      </span>
+    </div>
+  );
 };
 
 export default Task;
--- src/pages/stateTaskManager/TaskManager.js
+++ src/pages/stateTaskManager/TaskManager.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import "./TaskManager.css";
+import Task from "./Task";
 
 const TaskManager = () => {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [task, setTask] = useState([]);
+
+  const nameInputRef = useRef(null);
+
+  useEffect(() => {
+    nameInputRef.current.focus();
+  }, []);
+
   return (
     <div className="--bg-primary">
       <h1>Task Manager</h1>
@@ -9,16 +21,35 @@ const TaskManager = () => {
           <form action="" className="form --form-control">
             <div>
               <label htmlFor="name">Task:</label>
-              <input type="text" placeholder="Task name" />
+              <input
+                type="text"
+                placeholder="Task name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                ref={nameInputRef}
+              />
             </div>
             <div>
               <label htmlFor="name">Date:</label>
-              <input type="date" placeholder="Task date" />
+              <input
+                type="date"
+                placeholder="Task date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
             </div>
             <button className="--btn --btn-success --btn-block">
               Save Task
             </button>
           </form>
+        </div>
+      </div>
+
+      <div className="--flex-center --my2">
+        <div className="--width-500px --p">
+          <h2 className="--text-light">Task List</h2>
+          <hr style={{ background: "#fff" }} />
+          <Task />
         </div>
       </div>
     </div>
