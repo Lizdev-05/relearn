b559cbef512c779a5e352c574e91a91cd039ddf3
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-15 12:24:30 +0100

    Use useEffect to clear the close alert

--- src/pages/stateTaskManager/reducerTaskManager/alert/Alert.js
+++ src/pages/stateTaskManager/reducerTaskManager/alert/Alert.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Aler.css";
 import { FaExclamationCircle, FaTimes } from "react-icons/fa";
 
 const Alert = ({ alertContent, alertClass, onCloseAlert }) => {
+  useEffect(() => {
+    const int = setTimeout(() => {
+      onCloseAlert();
+    }, 3000);
+
+    return () => {
+      clearTimeout(int);
+    };
+  });
+
   return (
     <div className={`alert ${alertClass}`} onClick={onCloseAlert}>
       <FaExclamationCircle size={16} className="icon-x" />
