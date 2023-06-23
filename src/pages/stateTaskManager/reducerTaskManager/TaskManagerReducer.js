4d956dd29bf7279ef21e5bf8af5046c61aecf0eb
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-23 17:13:28 +0100

    Add the reduccer to anotther file

--- src/pages/stateTaskManager/reducerTaskManager/TaskManagerReducer.js
+++ src/pages/stateTaskManager/reducerTaskManager/TaskManagerReducer.js
@@ -6,107 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import useLocalStorage from "use-local-storage";
 import Alert from "./alert/Alert";
 import Confirm from "./confirm/Confirm";
-import { type } from "@testing-library/user-event/dist/type";
-
-const taskReducer = (state, action) => {
-  if (action.type === "EMPTY_FIELDS") {
-    return {
-      ...state,
-      isAlertOpen: true,
-      alertContent: "Kindly enter a name and a date",
-      alertClass: "danger",
-    };
-  }
-
-  if (action.type === "CLOSE_ALERT") {
-    return { ...state, isAlertOpen: false };
-  }
-
-  if (action.type === "ADD_TASK") {
-    const allTasks = [...state.tasks, action.payload];
-    return {
-      ...state,
-      tasks: allTasks,
-      isAlertOpen: true,
-      alertContent: "Task added successfully",
-      alertClass: "success",
-    };
-  }
-
-  if (action.type === "EDIT_TASK_MODAL") {
-    return {
-      ...state,
-      taskId: action.payload,
-      isEditModalOpen: true,
-      modalTitle: "Edit Task",
-      modalMsg: "You are about to edit this taks",
-      modalActionText: "Edit",
-    };
-  }
-
-  if (action.type === "EDIT_TASK") {
-    return {
-      ...state,
-      isEditing: true,
-    };
-  }
-
-  if (action.type === "CLOSE_MODAL") {
-    return {
-      ...state,
-      isEditModalOpen: false,
-      isDeleteModalOpen: false,
-    };
-  }
-
-  if (action.type === "UPDATE_TASK") {
-    const updatedTask = action.payload;
-    const id = action.payload.id;
-
-    const taskIndex = state.tasks.findIndex((task) => {
-      return task.id === id;
-    });
-
-    if (taskIndex !== -1) {
-      state.tasks[taskIndex] = updatedTask;
-    }
-
-    return {
-      ...state,
-      isEditing: false,
-      isAlertOpen: true,
-      alertContent: "Task updated successfully",
-      alertClass: "success",
-    };
-  }
-
-  if (action.type === "OPEN_DELETE_MODAL") {
-    console.log(action.payload);
-    return {
-      ...state,
-      taskId: action.payload,
-      isDeleteModalOpen: true,
-      modalTitle: "Delete Task",
-      modalMsg: "You are about to delete this task",
-      modalActionText: "Delete",
-    };
-  }
-
-  if (action.type === "DELETE_TASK") {
-    const id = action.payload;
-    const remainingTasks = state.tasks.filter((task) => task.id !== id);
-    return {
-      ...state,
-      tasks: remainingTasks,
-      isAlertOpen: true,
-      alertContent: "Task deleted successfully",
-      alertClass: "success",
-      isDeleteModalOpen: false,
-    };
-  }
-
-  return state;
-};
+import { taskReducer } from "./reducerTask";
 
 const TaskManagerReducer = () => {
   const [name, setName] = useState("");
@@ -235,7 +135,24 @@ const TaskManagerReducer = () => {
     setTasks(remainingTasks);
   };
 
-  const completeTaskFn = (id) => {};
+  const completeTaskFn = (id) => {
+    dispatch({
+      type: "COMPLETE_TASK",
+      payload: id,
+    });
+
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            completed: true,
+          };
+        }
+        return task;
+      })
+    );
+  };
   const closeModalfn = () => {
     dispatch({
       type: "CLOSE_MODAL",
