199f5f14d29ccb323d89afb09aed1b1d411414dc
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-24 15:55:30 +0100

    Add blog details

--- src/pages/route/router/BlogDetails.js
+++ src/pages/route/router/BlogDetails.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { blogs } from "./Data";
 
 const BlogDetails = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [details, setDetails] = useState("");
+
   const { id } = useParams();
+
+  useEffect(() => {
+    const thisBlog = blogs.find((blog) => blog.id === parseInt(id));
+    setTitle(thisBlog.title);
+    setAuthor(thisBlog.author);
+    setDetails(thisBlog.details);
+  });
   return (
     <div className="--center-all">
-      <h1 className="--text-center">BlogDetails {id}</h1>
+      <h1 className="--text-center"> {title}</h1>
+      <hr />
+      <p>
+        <b>Written by:</b> {author}
+      </p>
+      <br />
+      <p>{details}</p>
     </div>
   );
 };
