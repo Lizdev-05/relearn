97a4f6ff6425bac8d943d57ecd4e1871368e1528
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-28 09:53:16 +0100

    Create a reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,18 @@
+import { act } from "react-dom/test-utils";
 import { createStore } from "react-redux";
+
+const countReducer = (state = 0, action) => {
+  if (action.type === "SUBTRACTION") {
+    state - 1;
+  }
+  if (action.type === "RESET") {
+    state = 0;
+  }
+  if (action.type === "ADD") {
+    state + 1;
+  }
+  return state;
+};
 
 const store = createStore();
 
