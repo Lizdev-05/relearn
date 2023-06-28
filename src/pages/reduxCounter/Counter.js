f84775f50bdf6c7334652005bf598b67cc54e149
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-06-28 10:13:14 +0100

    Install redux toolkit

---
+++ src/pages/reduxCounter/Counter.js
@@ -0,0 +1,48 @@
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+  const count = useSelector();
+
+  const dispatch = useDispatch(count);
+
+  const addHandler = () => {
+    dispatch({
+      type: "ADD",
+    });
+  };
+  const subtractHandler = () => {
+    dispatch({
+      type: "SUBTRACT",
+    });
+  };
+  const resetHandler = () => {
+    dispatch({
+      type: "RESET",
+    });
+  };
+
+  return (
+    <section className="--flex-center --100vh --bg-primary">
+      <div className="container --bg-light --p2 --m2 --center-all">
+        <h1>Counter</h1>
+
+        <span>{count}</span>
+
+        <div className="buttons --flex-center">
+          <button className=" --btn --btn-primary" onClick={addHandler}>
+            Add
+          </button>
+
+          <button className=" --btn" onClick={resetHandler}>
+            Reset
+          </button>
+          <button className=" --btn --btn-danger" onClick={subtractHandler}>
+            Subtract
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
